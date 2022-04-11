544051efe7146b3ab5f14385573a5db6403fc38c
Aleksandar Vrencev <aleksandarvrencev@gmail.com>
2022-04-11 18:42:20 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+math = require('mathjs');
+
+var rounding = math.round(6, 3)
+var atan2 = math.atan2(3, -3)
+var logging = math.log(10000, 10)
+
+console.log(rounding + '\n' + atan2 + '\n' + logging)
